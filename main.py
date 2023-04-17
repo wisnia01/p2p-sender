12693e6f9156292e95a150de12879bc804f615eb
Jakub Waśniewski <wasniewski.jakub@wp.pl>
2023-04-17 18:55:56 +0200

    Add decrypt and encrypt cbc functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
 
 #AES Implementation
@@ -26,9 +27,28 @@ def decrypt_ebc(ciphertext, key):
     return plaintext
 
 
+def encrypt_cbc(plaintext, key):
+    iv = get_random_bytes(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plaintext = pad(plaintext, AES.block_size)
+    ciphertext = cipher.encrypt(padded_plaintext)
+
+    return iv, ciphertext
+
+def decrypt_cbc(ciphertext, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plaintext = cipher.decrypt(ciphertext)
+    plaintext = unpad(padded_plaintext, AES.block_size)
+
+    return plaintext
+
 def main():
     print("start")
     print(decrypt_ebc(encrypt_ebc(plaintext, key),key))
+
+    print("cbc now")
+    iv, cipher = encrypt_cbc(plaintext, key)
+    print(decrypt_cbc(cipher, key, iv))
     print("stop")
 
 if __name__ == "__main__":
